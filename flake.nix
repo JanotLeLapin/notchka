f040ab0167bbc640dc5c99ab5247fabd594a75e0
Joseph DALY <joseph300905@gmail.com>
2023-12-23 00:15:48 +0100

    use crate2nix

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
+    crate2nix.url = "github:kolloch/crate2nix";
     overlay.url = "github:oxalica/rust-overlay";
     systems.url = "github:nix-systems/default";
     flake-utils.url = "github:Numtide/flake-utils";
@@ -11,6 +12,7 @@
   outputs = {
     self,
     nixpkgs,
+    crate2nix,
     overlay,
     flake-utils,
     ...
@@ -21,26 +23,26 @@
     };
 
     manifest = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package;
-    generator = pkgs.rustPlatform.buildRustPackage {
-      name = manifest.name;
-      pname = manifest.name;
-      version = manifest.version;
-      src = ./.;
-
-      cargoBuildFlags = "--release";
-      cargoLock = { lockFile = ./Cargo.lock; };
-    };
-  in {
+    generator =
+      let
+        crate = pkgs.callPackage "${crate2nix}/tools.nix" { inherit pkgs; };
+      in import (crate.generatedCargoNix {
+        name = manifest.name;
+        src = ./.;
+      }) {
+        inherit pkgs;
+      };
+  in rec {
     devShells.default = pkgs.mkShell {
       packages = [ (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml) ];
     };
-    packages.generator = generator;
+    packages.generator = generator.rootCrate.build;
     packages.default = pkgs.stdenv.mkDerivation {
       name = "uni";
       version = manifest.version;
       src = ./.;
 
-      buildInputs = [ generator ];
+      buildInputs = [ packages.generator ];
       buildPhase = "generator";
       installPhase = "cp -r dist $out";
     };
