c3d821f41322418aa28ba66fba9315572e33f0a9
Joseph DALY <joseph300905@gmail.com>
2023-12-23 16:10:21 +0100

    allow multiple stylesheet syntaxes

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod md;
 mod html;
 mod util;
 
+use colored::*;
+
 const OUT: &str = "build";
 
 #[derive(Debug, Default, serde::Deserialize)]
@@ -19,11 +21,15 @@ pub struct Page {
 }
 
 fn main() -> std::io::Result<()> {
-    std::fs::remove_dir_all(OUT)?;
+    let start = std::time::Instant::now();
+
+    let _ = std::fs::remove_dir_all(OUT);
 
     for file in util::walk_dir("content") {
         let content = std::fs::read_to_string(&file.path)?;
+        let now = std::time::Instant::now();
         let page = html::make_page(md::parse_markdown(&content));
+        println!("{} page '{}' (in {:?})", "Compiled".green(), file.path, now.elapsed());
 
         let out = std::path::Path::new(OUT).join(file.path.replace("content/", ""));
         let out = out.parent().unwrap();
@@ -31,14 +37,31 @@ fn main() -> std::io::Result<()> {
         std::fs::write(out.join(format!("{}.html", file.name)), page)?;
     }
 
-    let options = grass::Options::default().style(grass::OutputStyle::Compressed);
     for file in util::walk_dir("style") {
-        if let Ok(result) = grass::from_path(&file.path, &options) {
-            let out = std::path::Path::new(OUT).join("dist").join(&file.path);
-            let out = out.parent().unwrap();
-            std::fs::create_dir_all(out)?;
-            std::fs::write(out.join(format!("{}.css", file.name)), result)?;
-        }
+        let syntax = match file.ext.as_str() {
+            "css" => grass::InputSyntax::Css,
+            "sass" => grass::InputSyntax::Sass,
+            "scss" => grass::InputSyntax::Scss,
+            _ => continue,
+        };
+        let options = grass::Options::default()
+            .style(grass::OutputStyle::Compressed)
+            .quiet(true)
+            .input_syntax(syntax);
+
+        let now = std::time::Instant::now();
+        match grass::from_path(&file.path, &options) {
+            Ok(result) => {
+                println!("{} stylesheet '{}' (in {:?})", "Compiled".green(), file.path, now.elapsed());
+                let out = std::path::Path::new(OUT).join("dist").join(&file.path);
+                let out = out.parent().unwrap();
+                std::fs::create_dir_all(out)?;
+                std::fs::write(out.join(format!("{}.css", file.name)), result)?;
+            },
+            Err(err) => {
+                println!("{} for stylesheet '{}': {}", "Compilation failed".red(), file.path, err);
+            }
+        };
     }
 
     for file in util::walk_dir("dist") {
@@ -46,6 +69,8 @@ fn main() -> std::io::Result<()> {
         std::fs::create_dir_all(out.parent().unwrap())?;
         std::fs::copy(&file.path, out)?;
     }
+
+    println!("{} (took {:?})", "Done".blue(), start.elapsed());
 
     Ok(())
 }
