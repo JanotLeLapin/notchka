42fd48864dd33d74e5e89041c1f49472d7b5bb05
Joseph DALY <joseph300905@gmail.com>
2023-12-23 22:25:48 +0100

    implement into File for String

--- src/util.rs
+++ src/util.rs
@@ -5,6 +5,35 @@ pub struct File {
     pub path: String,
 }
 
+impl Into<File> for String {
+    fn into(self) -> File {
+        let file_name = self.split("/").last().unwrap();
+
+        let mut iter = file_name.split(".");
+        let mut name = String::new();
+        let mut ext = String::new();
+
+        while let Some(elem) = iter.next() {
+            name.push_str(&ext);
+            ext = elem.to_string();
+        }
+
+        if name.is_empty() {
+            File {
+                name: ext,
+                ext: name,
+                path: self,
+            }
+        } else {
+            File {
+                name,
+                ext,
+                path: self,
+            }
+        }
+    }
+}
+
 pub fn walk_dir(path: &str) -> Vec<File> {
     let mut res = Vec::new();
     for entry in walkdir::WalkDir::new(path) {
@@ -13,22 +42,7 @@ pub fn walk_dir(path: &str) -> Vec<File> {
                 continue;
             }
 
-            if let Some(file_name) = file.file_name().to_str() {
-                let mut iter = file_name.split(".");
-                let mut name = String::new();
-                let mut ext = String::new();
-
-                while let Some(elem) = iter.next() {
-                    name.push_str(&ext);
-                    ext = elem.to_string();
-                }
-
-                res.push(File {
-                    name,
-                    ext,
-                    path: file.path().to_str().unwrap().to_string(),
-                })
-            }
+            res.push(file.path().to_str().unwrap().to_string().into())
         }
     }
     res
